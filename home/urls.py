55b320d43d1e55a6c5a701ce79e618ab3c8c16ce
unite3 <kumar.manish12jan@gmail.com>
2022-01-08 02:32:14 +0530

    started django project & added auth login logout

---
+++ home/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+from home import views
+admin.site.site_header = "TableApp admin"
+admin.site.site_title = "TableApp admin"
+admin.site.index_title = "Welcome to TableApp Admin"
+urlpatterns = [
+
+    path('',views.index,name="home"),
+    path('login',views.loginuser,name="login"),
+    path('logout',views.logoutuser,name="logout"),
+]
